493d8141e7f58f18b90bd6fca5c41e99040b66c2
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2022-11-22 12:20:24 +0100

    added css changes

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const btn = document.querySelector('button');
-const section = document.querySelector('section');
+const startBtn = document.querySelector('.dont-btn');
+const section = document.querySelector('.starting-page-active');
 const main = document.querySelector('main');
 const mainh2 = main.querySelector('h2');
-// btn.addEventListener('click', () => {
-//   section.classList.add('section-active');
-//   btn.classList.add('button-active');
-//   btn.textContent = 'WHY';
-// });
+const mainbtn = main.querySelector('button');
+
 const mainLocation = 1000;
 let currentScroll = 0;
-btn.addEventListener('click', () => {
+startBtn.addEventListener('click', () => {
   main.classList.add('main-open');
   mainh2.classList.add('main-open-h2');
+  mainbtn.classList.add('main-btn-active');
+  mainbtn.classList.remove('main-btn');
   window.scrollTo({
     top: 1000,
     behavior: 'smooth',
   });
   setTimeout(() => {
-    section.classList.remove('section-active');
+    section.classList.remove('starting-page-active');
   }, 1000);
 });
